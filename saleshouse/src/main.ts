b1d15bef0143e08ff13b1e2e0484973d71a29a7a
Carlos <marrugovargascarlos@gmail.com>
2024-03-26 01:48:11 -0500

    Actualize el index.html agregando la estrucuta para crear in header con los botones, barra de busqueda y un icono para la app, actualize el main.ts para agregar el efecto slider a tres imagenes que puse en assets/imagenes para que se muevan solas estilo promocion

--- saleshouse/src/main.ts
+++ saleshouse/src/main.ts
@@ -2,5 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+let slideIndex: number = 0;
+showSlides();
+
+function showSlides(): void {
+    const slides: NodeListOf<HTMLImageElement> = document.querySelectorAll('.slides img') as NodeListOf<HTMLImageElement>;
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = 'block';  
+    setTimeout(showSlides, 2000); // Cambia la imagen cada 2 segundos
+}
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
