a2cb6694dd47375fc44634058d546d3c480364c5
Carlos <marrugovargascarlos@gmail.com>
2024-03-29 17:57:50 -0500

    Se ha instalado primeng

--- saleshouse/src/main.ts
+++ saleshouse/src/main.ts
@@ -4,5 +4,5 @@ import { AppComponent } from './app/app.component';
 
 
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err));
